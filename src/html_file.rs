e542b562744d4e9756e3e938db6dc70863e65ff5
Ronald Baldwin <robaldwin9@gmail.com>
2020-12-02 19:48:29 -0500

    Add filter buttons  - modify css  - Add button tag  - Add h3 tag  - Add js file

---
+++ src/html_file.rs
@@ -0,0 +1,184 @@
+/**
+TODO:
+* Add ability to have multiple properties in a tag
+* Add rest of header tags
+
+
+
+*/
+
+const GREATER_THAN: &str = ">";
+
+pub enum TagType {
+    DIV,
+    P,
+    HTML,
+    ARTICLE,
+    HEAD,
+    SCRIPT,
+    FOOTER,
+    BODY,
+    BUTTON,
+    H3,
+}
+
+impl TagType {
+    pub fn open_tag(&self) -> String {
+        match *self {
+            TagType::DIV => "<div".to_string(),
+            TagType::P => "<p".to_string(),
+            TagType::HTML => "<html".to_string(),
+            TagType::ARTICLE => "<article".to_string(),
+            TagType::HEAD => "<head".to_string(),
+            TagType::SCRIPT => "<script".to_string(),
+            TagType::FOOTER => "<footer".to_string(),
+            TagType::BODY => "<body".to_string(),
+            TagType::BUTTON => "<button".to_string(),
+            TagType::H3 => "<h3".to_string(),
+        }
+    }
+
+    pub fn close_tag(&self) -> String {
+        match *self {
+            TagType::DIV => "</div>".to_string(),
+            TagType::P => "</p>".to_string(),
+            TagType::HTML => "</html>".to_string(),
+            TagType::ARTICLE => "</article>".to_string(),
+            TagType::HEAD => "</head>".to_string(),
+            TagType::SCRIPT => "</script>".to_string(),
+            TagType::FOOTER => "</footer>".to_string(),
+            TagType::BODY => "</body>".to_string(),
+            TagType::BUTTON => "</button>".to_string(),
+            TagType::H3 => "</h3>".to_string(),
+        }
+    }
+}
+
+use std::fmt::{self, Display, Formatter};
+#[derive(Copy, Clone)]
+pub struct Property {
+    pub propertyname: &'static str,
+    pub propertyvalue: &'static str,
+}
+
+// struct for html tag
+pub struct Tag {
+    pub properties: Property,
+    pub tagtype: TagType,
+    pub content: String,
+    pub children: Vec<Tag>,
+}
+
+impl Property {
+    pub fn new(propertyname: &'static str, propertyvalue: &'static str) -> Property {
+        Property {
+            propertyname,
+            propertyvalue,
+        }
+    }
+}
+
+impl Display for Property {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        if self.propertyname.len() > 0 && self.propertyvalue.len() > 0 {
+            write!(f, " {}={}", self.propertyname, self.propertyvalue)
+        } else {
+            write!(f, "")
+        }
+    }
+}
+
+// impliment functions for tag struct
+impl Tag {
+    pub fn new(properties: Property, tagtype: TagType, content: String) -> Tag {
+        Tag {
+            properties,
+            tagtype,
+            content,
+            children: vec![],
+        }
+    }
+
+    pub fn display(&self) {
+        println!(
+            "{}{}{}",
+            self.tagtype.open_tag(),
+            self.properties,
+            GREATER_THAN
+        );
+
+        for item in self.children.iter() {
+            item.display();
+        }
+        println!("{}{}", self.content, self.tagtype.close_tag());
+    }
+}
+
+impl Display for Tag {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}{}{}",
+            self.tagtype.open_tag(),
+            self.properties,
+            GREATER_THAN
+        )?;
+
+        for child in self.children.iter() {
+            write!(f, "{}", child.to_string())?;
+        }
+        write!(f, "{}{}", self.content, self.tagtype.close_tag())
+    }
+}
+
+// struct for an html doc
+pub struct HtmlDoc {
+    pub script: String,
+    pub style: String,
+    pub tags: Vec<Tag>,
+}
+
+// impliment functions for HtmlDoc struct
+impl HtmlDoc {
+    pub fn new() -> HtmlDoc {
+        HtmlDoc {
+            tags: vec![],
+            script: "".to_string(),
+            style: "".to_string(),
+        }
+    }
+    pub fn display(&self) {
+        for tag in self.tags.iter() {
+            tag.display();
+        }
+    }
+}
+
+impl fmt::Display for HtmlDoc {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}{}{}{}{}{}{}{}{}",
+            TagType::HTML.open_tag(),
+            GREATER_THAN,
+            TagType::HEAD.open_tag(),
+            GREATER_THAN,
+            self.style,
+            self.script,
+            TagType::HEAD.close_tag(),
+            TagType::BODY.open_tag(),
+            GREATER_THAN
+        )?;
+
+        for i in &self.tags {
+            write!(f, "{}", i.to_string())?;
+        }
+        write!(
+            f,
+            "{}{}",
+            TagType::BODY.close_tag(),
+            TagType::HTML.close_tag()
+        )?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ fn main() {
     // Get input file path
     let args: Vec<String> = env::args().collect();
     let mut html_doc = HtmlDoc::new();
+    html_doc.style = "<link rel=\"stylesheet\" href=\"src/css/rshtml.css\">".to_string();
+    html_doc.script = "<script src=\"src/js/YourExternalJQueryScripts.js\"></script>".to_string();
     let filename = args[0].clone();
 
     // String constants
     const CLASS: &str = "class";
-    const WARN:  &str = "warn";
+    const WARN: &str = "warn";
     const ERROR: &str = "error";
     const DEBUG: &str = "debug";
-    const INFO:  &str = "info";
+    const INFO: &str = "info";
 
     // Html properties, or classes applied based on log level */
     let warn_class = Property::new(CLASS, WARN);
@@ -23,27 +25,54 @@ fn main() {
     let debug_class = Property::new(CLASS, DEBUG);
     let info_class = Property::new(CLASS, INFO);
 
+    let mut warn_count = 0;
+    let mut error_count = 0;
+    let mut debug_count = 0;
+    let mut info_count = 0;
+
+    let filter_header = Tag::new(Property::new("", ""), TagType::H3, "filters".to_string());
+    html_doc.tags.push(filter_header);
+    let warn_button = Tag::new(warn_class, TagType::BUTTON, "Warn".to_string());
+    let error_button = Tag::new(error_class, TagType::BUTTON, "Error".to_string());
+    let debug_button = Tag::new(debug_class, TagType::BUTTON, "Debug".to_string());
+    let info_button = Tag::new(info_class, TagType::BUTTON, "Info".to_string());
+    html_doc.tags.push(debug_button);
+    html_doc.tags.push(warn_button);
+    html_doc.tags.push(error_button);
+    html_doc.tags.push(info_button);
+
     // Parse input, and create htmldoc tags
-    if let Ok(lines) = read_lines("LICENSE") {
+    if let Ok(lines) = read_lines("test.log") {
         for line in lines {
             if let Ok(log_line) = line {
                 let lower_log_line = log_line.to_lowercase().clone();
                 println!("{}", lower_log_line);
 
                 if log_line.contains(WARN) {
+                    if warn_count == 0 {
+                        warn_count += 1;
+                    }
                     add_log_level_tag(warn_class, lower_log_line, &mut html_doc);
                 } else if log_line.contains(ERROR) {
+                    if error_count == 0 {
+                        error_count += 1;
+                    }
                     add_log_level_tag(error_class, lower_log_line, &mut html_doc);
                 } else if log_line.contains(DEBUG) {
+                    if debug_count == 0 {
+                        debug_count += 1;
+                    }
                     add_log_level_tag(debug_class, lower_log_line, &mut html_doc);
                 } else if lower_log_line.is_empty() {
                 } else {
+                    if info_count == 0 {
+                        info_count += 1;
+                    }
                     add_log_level_tag(info_class, lower_log_line, &mut html_doc)
                 }
             }
         }
     }
-
 
     let mut name: &str;
     // Universal directory seperator
